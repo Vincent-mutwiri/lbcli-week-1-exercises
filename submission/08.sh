7db31cc014284deb0081a36ff70465d483bbc8ce
Vincent Mutwiri <vincentmutwiri9@gmail.com>
2026-03-20 20:44:41 +0300

    Fix typo in SegWit address format from 'bench32' to 'bech32'

--- submission/08.sh
+++ submission/08.sh
@@ -1,2 +1,2 @@
 # Generate a native SegWit address.
-bitcoin-cli -regtest -rpcwallet=builderswallet getnewaddress "" "bench32"
+bitcoin-cli -regtest -rpcwallet=builderswallet getnewaddress "" "bech32"
